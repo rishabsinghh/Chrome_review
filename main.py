cfe400b5aaca356be4e7cd02f6e5fc04b7085df3
rishabsinghh <rishabsingh240@gmail.com>
2022-05-11 19:52:07 +0530

    Final Commit

--- main.py
+++ main.py
@@ -13,15 +13,16 @@ def main():
         predictor=PredictionFunc(file_object,log_writer)
         st.title("Chrome Reviews Problem")
         uploaded_file = st.file_uploader("Upload your data here", type=["csv"])
+        path_to_store=st.text_input("Enter the path to store the output file")
         if uploaded_file is not None:
                 data = pd.read_csv(uploaded_file)
                 st.subheader("Input Data")
                 st.write(data)
                 if st.button("Predict"):
                     result = predictor.predict(data)
-                    result.to_csv(uploaded_file+"//"+"predicted_data.csv")
+                    result.to_csv(str(path_to_store)+"//"+"predicted_data.csv")
                     st.subheader("Predictions")
-                    st.write("File created at: "+ str(uploaded_file)+"/predicted_data.csv")
+                    st.write("File created at: "+ str(path_to_store)+"\predicted_data.csv")
                     st.success("Predictions done")
                     log_writer.log(file_object,"Predictions done")
 
