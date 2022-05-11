91a8cb759daf8a30ad1c9302b2fd699ca28c6ba4
rishabsinghh <rishabsingh240@gmail.com>
2022-05-11 22:50:15 +0530

    New commit

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from application_logger.logger import app_logger
 import streamlit as st
 import pandas as pd
 import numpy as np
-import os
+import streamlit_authenticator as stauth
+
 file_object = open("training_logs/logs.txt",'a+')
 log_writer=app_logger()
 def main():
@@ -11,16 +12,16 @@ def main():
     try:
         log_writer.log(file_object,"Application Started")
         predictor=PredictionFunc(file_object,log_writer)
-        st.title("Chrome Reviews Problem")
+        st.title("Chrome Reviews Problem")#Defining the Header for the page
         uploaded_file = st.file_uploader("Upload your data here", type=["csv"])
         if uploaded_file is not None:
-                data = pd.read_csv(uploaded_file)
+                data = pd.read_csv(uploaded_file)#Reading the uploaded file
                 st.subheader("Input Data")
                 st.write(data)
                 if st.button("Predict"):
-                    result = predictor.predict(data)
-                    st.write(result)
-                    st.success("Predictions done")
+                    result = predictor.predict(data)#Calling the predict function
+                    st.success("Predictions done, Output:")
+                    st.write(result)#Writing the output
                     log_writer.log(file_object,"Predictions done")
 
         else:
@@ -31,4 +32,17 @@ def main():
     log_writer.log(file_object,"Exited from main function")
     file_object.close()
 if __name__=="__main__":
-    main()
+    #Authentication
+    names = ['VIP']
+    usernames = ['evaluator']
+    passwords = ['please pass me to the next round']
+    hashed_passwords = stauth.Hasher(passwords).generate()
+    authenticate=stauth.Authenticate(names, usernames, hashed_passwords,'bread','1234',cookie_expiry_days=30)
+    name, authentication_status, username = authenticate.login('Login','main')
+    if authentication_status:
+        authenticate.logout('Logout','main')
+        main()
+    elif authentication_status == False:
+        st.error('Username/password is incorrect')
+    elif authentication_status == None:
+        st.warning('Please enter your username and password')
